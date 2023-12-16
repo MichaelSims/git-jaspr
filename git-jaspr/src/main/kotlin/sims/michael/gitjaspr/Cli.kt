5e3817dbe65bc31dac4c7d3ddb504286d9f58d6d
Michael Sims <michael.sims@trillianthealth.com>
2023-12-16 16:49:53 -0600

    Include --version option
    There's gotta be a better way to do this than manually, but this will
    do for now.
    
    For https://github.com/MichaelSims/git-jaspr/issues/108
    
    commit-id: I5f8995bd

--- git-jaspr/src/main/kotlin/sims/michael/gitjaspr/Cli.kt
+++ git-jaspr/src/main/kotlin/sims/michael/gitjaspr/Cli.kt
@@ -395,6 +395,7 @@ object Cli {
         is handled via an abstract base class.
          */
         NoOpCliktCommand(name = "git jaspr")
+            .versionOption("v4-beta")
             .subcommands(
                 listOf(
                     Status(),
