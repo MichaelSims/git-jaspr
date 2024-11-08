6be46170bd4a081c88e1e06bfdd28cfa898c0870
Michael Sims <michael.sims@trillianthealth.com>
2024-11-08 14:35:27 -0600

    ErrorMappingGraphQLClient throws if the response contains any errors
    commit-id: I4d1344c4

--- git-jaspr/src/main/kotlin/sims/michael/gitjaspr/ErrorMappingGraphQLClient.kt
+++ git-jaspr/src/main/kotlin/sims/michael/gitjaspr/ErrorMappingGraphQLClient.kt
@@ -1,6 +1,7 @@
 package sims.michael.gitjaspr
 
 import com.expediagroup.graphql.client.GraphQLClient
+import com.expediagroup.graphql.client.types.GraphQLClientError
 import com.expediagroup.graphql.client.types.GraphQLClientRequest
 import com.expediagroup.graphql.client.types.GraphQLClientResponse
 import io.ktor.client.plugins.ClientRequestException
@@ -13,25 +14,34 @@ class ErrorMappingGraphQLClient<RequestCustomizer>(
         request: GraphQLClientRequest<T>,
         requestCustomizer: RequestCustomizer.() -> Unit,
     ): GraphQLClientResponse<T> = try {
-        delegate.execute(request, requestCustomizer)
+        delegate.execute(request, requestCustomizer).also(::failIfAnyErrors)
     } catch (e: ClientRequestException) {
-        mapException(e)
+        mapClientRequestException(e)
     }
 
     override suspend fun execute(
         requests: List<GraphQLClientRequest<*>>,
         requestCustomizer: RequestCustomizer.() -> Unit,
     ): List<GraphQLClientResponse<*>> = try {
-        delegate.execute(requests, requestCustomizer)
+        delegate.execute(requests, requestCustomizer).onEach(::failIfAnyErrors)
     } catch (e: ClientRequestException) {
-        mapException(e)
+        mapClientRequestException(e)
     }
 
-    private fun mapException(e: ClientRequestException): Nothing {
+    private fun mapClientRequestException(e: ClientRequestException): Nothing {
         if (e.response.status == HttpStatusCode.Unauthorized) {
             throw GitJasprException("GitHub authorization failed, please check your token", e)
         } else {
             throw e
+        }
+    }
+
+    private fun failIfAnyErrors(response: GraphQLClientResponse<*>) {
+        val errors = response.errors.orEmpty()
+        if (errors.isNotEmpty()) {
+            throw GitJasprException(
+                "GraphQL request failed with errors: ${errors.map(GraphQLClientError::message)}",
+            )
         }
     }
 }
